8e6af324cddb69b62ff924b4dc3f2ce33a33c23a
jnunez0319 <98064227+jnunez0319@users.noreply.github.com>
2022-01-27 17:40:37 -0500

    Update dbcreation.sql

--- database/dbcreation.sql
+++ database/dbcreation.sql
@@ -25,18 +25,3 @@ CREATE TABLE `COP4331`.`Contacts`
   `UserID` INT NOT NULL DEFAULT '0' , 
   PRIMARY KEY (`ID`)
 ) ENGINE = InnoDB;
-
-USE COP4331;
-
-/* Populate sample Users */ 
-insert into Users (FirstName,LastName,Login,Password) VALUES ('Rick','Leinecker','RickL','COP4331');
-insert into Users (FirstName,LastName,Login,Password) VALUES ('Sam','Hill','SamH','Test');
-insert into Users (FirstName,LastName,Login,Password) VALUES ('Rick','Leinecker','RickL','5832a71366768098cceb7095efb774f2');
-insert into Users (FirstName,LastName,Login,Password) VALUES ('Sam','Hill','SamH','0cbc6611f5540bd0809a388dc95a615b');
-
-/* Populate sample Contacts (Some Information may or may not be available) */
-insert into Contacts (FirstName,LastName,Email,Phone,UserID) VALUES ('John','Doe', 'JD00@test.com','0000000000', 3);
-insert into Contacts (FirstName,LastName,Email,UserID) VALUES ('J','H', 'a@h.com', 0);
-insert into Contacts (FirstName,Email,Phone,UserID) VALUES ('Sam', 'sammyT@test.com','0000000001', 1);
-insert into Contacts (FirstName,LastName,Email,Phone,UserID) VALUES ('John','Doe', 'JD00@test.com','0000000000', 2);
-insert into Contacts (FirstName,LastName,Email,UserID) VALUES ('Barry','Allen', 'def-not@theflash.com', 4);
