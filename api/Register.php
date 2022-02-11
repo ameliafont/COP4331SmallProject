26c932dd038f817c738c4caa302bf97a530c59e5
SoupyOfficial <59628901+SoupyOfficial@users.noreply.github.com>
2022-02-10 19:31:42 -0500

    Update Register.php

--- api/Register.php
+++ api/Register.php
@@ -6,34 +6,40 @@
 	$login = $inData["Login"];
 	$password = $inData["Password"];
 	
-	if(empty($inData["FirstName"]) 
-	   || empty($inData["LastName"])
-	   || empty($inData["Login"])
-	   || empty($inData["Password"])) 
-	{returnWithError("Ensure all fields are filled");}
-
-	
-			 
-	$conn = new mysqli("localhost", "DatabaseUser", "DatabasePassword", "COP4331");
-	if ($conn->connect_error) 
+	if ($firstName == "" || $lastName == "" || $login == "" || $password == "")
 	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$exists = mysql_query($conn, "SELECT * FROM Users WHERE Login = '$login'"); 
-		if(mysql_num_rows($exists)>0)
-		{
-			returnWithError("Login already exists");
-		}
-		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("Successfully completed");
+		returnWithError("Ensure all fields are filled");
 	}
 
+	else 
+	{
+		$conn = new mysqli("localhost", "DatabaseUser", "DatabasePassword", "COP4331");
+		if ($conn->connect_error) 
+		{
+			returnWithError( $conn->connect_error );
+		} 
+		else
+		{
+			$check =  $conn->prepare("SELECT Login FROM Users WHERE Login =?");
+			$check->bind_param("s", $login);
+			$check->execute();
+			$res = $check->get_result();
+			$row = $res->fetch_assoc();
+			$check->close();
+			if($row["Login"] != "")
+			{
+				$conn->close();
+				returnWithError("Login already exists");
+			}
+			
+			$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?, ?, ?, ?)");
+			$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			returnWithError("Successfully completed");
+		}
+	}
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
